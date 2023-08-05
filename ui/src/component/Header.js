ae10fe1ea16e4c8bfff3f42583465f90d9f2bde1
oman765 <135813279+oman765@users.noreply.github.com>
2023-08-05 13:20:50 +0600

    Update Header.js

--- ui/src/component/Header.js
+++ ui/src/component/Header.js
@@ -35,7 +35,6 @@ const Header = () => {
         <div className="flex items-center gap-4 md:gap-7">
           <nav className="gap-4 md:gap-6 text-base md:text-lg hidden md:flex">
             <Link to={""}>Home</Link>
-            <Link to={"menu/63f0fdbb3bcc2f97fa53d25d"}>Menu</Link>
             <Link to={"about"}>About</Link>
             <Link to={"contact"}>Contact</Link>
           </nav>
