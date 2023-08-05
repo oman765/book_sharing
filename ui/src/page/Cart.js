10eb0d587460edd5cfc039cb45957ed29da4e79d
oman765 <135813279+oman765@users.noreply.github.com>
2023-08-05 13:24:23 +0600

    Update Cart.js

--- ui/src/page/Cart.js
+++ ui/src/page/Cart.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import emptyCartImage from "../assest/empty.gif";
 import CartProduct from "../component/cartProduct";
-//import {loadStripe} from '@stripe/stripe-js';
 import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
@@ -21,34 +20,6 @@ const Cart = () => {
 
   
   
-//   const handlePayment = async()=>{
-
-//       if(user.email){
-          
-//          // const stripePromise = await loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
-//           const res = await fetch(`${process.env.REACT_APP_SERVER_DOMIN}/create-checkout-session`,{
-//             method : "POST",
-//             headers  : {
-//               "content-type" : "application/json"
-//             },
-//             body  : JSON.stringify(productCartItem)
-//           })
-//           if(res.statusCode === 500) return;
-
-//           const data = await res.json()
-//           console.log(data)
-
-//           toast("Redirect to payment Gateway...!")
-//          // stripePromise.redirectToCheckout({sessionId : data}) 
-//       }
-//       else{
-//         toast("You have not Login!")
-//         setTimeout(()=>{
-//           navigate("/login")
-//         },1000)
-//       }
-    
-//   }
   return (
     <>
     
