004fbcd5a2367a229f93e3b2a9a98e726388b4f3
oman765 <135813279+oman765@users.noreply.github.com>
2023-08-09 11:43:28 +0600

    Update About.js

--- ui/src/page/About.js
+++ ui/src/page/About.js
@@ -5,15 +5,11 @@ const About = () => {
     <div className="container mx-auto py-8">
     <h1 className="text-3xl font-semibold mb-4">About Us</h1>
     <p className="text-lg">
-      Welcome cuet based book sharing website! Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Nulla vitae massa id nunc pulvinar euismod. Phasellus eleifend ipsum eget libero
-      finibus, eu vestibulum ligula rhoncus. Etiam et ultrices libero. Sed nec libero vel est
-      pharetra tincidunt eu et felis.
+      Welcome cuet based book sharing website! Here you can share your unnecessary book and borrow  necessary  book from other user .
     </p>
     <p className="text-lg mt-4">
       Our goal is to sharing book in cuet campus . If you share book our website can offer a wide range of
-      book from various categories, including eee,cse,me and more. Lorem ipsum
-      dolor sit amet, consectetur adipiscing elit.
+      book from various categories, including eee,cse,me and more.By sharing book help others to enriched knowladge.
     </p>
   </div>
   )
